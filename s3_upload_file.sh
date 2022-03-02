03f1e021c9f5efc270944bf7baea362c2c5918d6
kj0701 <appletalk@nate.com>
2022-03-02 10:21:06 +0900

    s3 file upload curl command

---
+++ s3_upload_file.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+./idpass.txt
+
+dateValue=`date -R`
+
+outputFile="s3test.txt"
+amzFile="test.txt"
+bucket="kj0701-test1"
+resource="/${bucket}/${amzFile}"
+contentType="application/x-compressed-tar"
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+
+curl -v -X PUT -T ${outputFile} \
+     -H "Host: ${bucket}.s3.amazonaws.com" \
+     -H "Date:  ${dateValue}" \
+	  -H "Content-Type: ${contentType}" \
+	  -H "Authorization : AWS ${s3Key} : ${signature}" \
+	   https://${bucket}.s3.amazonaws.com 
